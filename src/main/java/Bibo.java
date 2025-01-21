882e9a8b4aa62a07f48026411f4c6a54b89fd7e7
iuhiah <leehaihui2004@gmail.com>
2025-01-21 23:29:37 +0800

    Add to-do list

--- src/main/java/Bibo.java
+++ src/main/java/Bibo.java
@@ -4,26 +4,55 @@ public class Bibo {
     public static void speak(String message) {
         System.out.println("\n---------- Bibo says: ----------");
         System.out.println(message);
-
-        if (message.equals("Bye. Hope to see you again soon!")) {
-            return;
-        }
         System.out.println("\n----------- You say: -----------");
+    }
+
+    // overloading for output formatting
+    public static void speak(String message, boolean start, boolean end) {
+        if (start) {
+            System.out.println("\n---------- Bibo says: ----------");
+        }
+        
+        System.out.println(message);
+
+        if (end) {
+            System.out.println("\n----------- You say: -----------");
+        }
+    }
+
+    private static String[] todolist = new String[100];
+    private static int todolistCurr = 0;
+
+    public static void add(String task) {
+        todolist[todolistCurr] = task;
+        todolistCurr++;
+
+        speak("I've added [" + task + "] to your to-do list! What's next?");
+    }
+
+    public static void showList() {
+        speak("Here is your current to-do list:", true, false);
+        for (int i = 0; i < todolistCurr; i++) {
+            System.out.println((i + 1) + ". " + todolist[i]);
+        }
+        speak("What's next?", false, true);
     }
 
     public static void main(String[] args) {
         speak("Hello! I'm Bibo.\nWhat can I do for you?");
 
-        // echo input
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                showList();
+                continue;
             }
-            speak(input);
+            add(input);
         }
 
-        speak("Bye. Hope to see you again soon!");
+        speak("Bye. Hope to see you again soon!", true, false);
     }
 }
