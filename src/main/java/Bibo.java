36fc499b056e1599092197776023265a75384b3b
iuhiah <leehaihui2004@gmail.com>
2025-01-21 23:07:19 +0800

    Echo user input

--- src/main/java/Bibo.java
+++ src/main/java/Bibo.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Bibo {
+    public static void speak(String message) {
+        System.out.println("\n---------- Bibo says: ----------");
+        System.out.println(message);
+
+        if (message.equals("Bye. Hope to see you again soon!")) {
+            return;
+        }
+        System.out.println("\n----------- You say: -----------");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Bibo");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        speak("Hello! I'm Bibo.\nWhat can I do for you?");
+
+        // echo input
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            speak(input);
+        }
+
+        speak("Bye. Hope to see you again soon!");
     }
 }
