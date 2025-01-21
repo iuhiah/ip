4b460e58a4e45c64bf9ecb475053c439c99dcd61
iuhiah <leehaihui2004@gmail.com>
2025-01-22 00:01:00 +0800

    Add Task class, message output cleanup

--- src/main/java/Bibo.java
+++ src/main/java/Bibo.java
@@ -2,57 +2,81 @@ import java.util.Scanner;
 
 public class Bibo {
     public static void speak(String message) {
-        System.out.println("\n---------- Bibo says: ----------");
         System.out.println(message);
-        System.out.println("\n----------- You say: -----------");
     }
 
-    // overloading for output formatting
-    public static void speak(String message, boolean start, boolean end) {
-        if (start) {
-            System.out.println("\n---------- Bibo says: ----------");
-        }
-        
-        System.out.println(message);
-
-        if (end) {
-            System.out.println("\n----------- You say: -----------");
-        }
-    }
-
-    private static String[] todolist = new String[100];
+    private static Task[] todolist = new Task[100];
     private static int todolistCurr = 0;
 
     public static void add(String task) {
-        todolist[todolistCurr] = task;
+        todolist[todolistCurr] = new Task(task);
         todolistCurr++;
 
-        speak("I've added [" + task + "] to your to-do list! What's next?");
+        speak("I've added [" + task + "] to your to-do list!");
     }
 
     public static void showList() {
-        speak("Here is your current to-do list:", true, false);
+        speak("Here is your to-do list:");
+
         for (int i = 0; i < todolistCurr; i++) {
-            System.out.println((i + 1) + ". " + todolist[i]);
+            speak((i + 1) + ". " + todolist[i]);
         }
-        speak("What's next?", false, true);
+    }
+
+    public static void markTask(int index) {
+        if (index < 1 || index > todolistCurr) {
+            speak("That's not in your to-do list! Try something else.");
+            return;
+        }
+
+        todolist[index - 1].markAsDone();
+        speak("Nice! I've marked this task as done:");
+        speak(todolist[index - 1].toString());
+    }
+
+    public static void unmarkTask(int index) {
+        if (index < 1 || index > todolistCurr) {
+            speak("That's not in your to-do list! Try something else.");
+            return;
+        }
+
+        todolist[index - 1].markAsUndone();
+        speak("Alright! I've unmarked this task:");
+        speak(todolist[index - 1].toString());
     }
 
     public static void main(String[] args) {
         speak("Hello! I'm Bibo.\nWhat can I do for you?");
 
         while (true) {
+            speak("\n----------- You say: -----------");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
+            speak("\n---------- Bibo says: ----------");
+
             if (input.equals("bye")) {
                 break;
-            } else if (input.equals("list")) {
-                showList();
-                continue;
             }
-            add(input);
+            
+            if (input.equals("list")) {
+                showList();
+            } else if (input.startsWith("mark")) {
+                String[] words = input.split(" ");
+                int index = Integer.parseInt(words[1]);
+                markTask(index);
+            } else if (input.startsWith("unmark")) {
+                String[] words = input.split(" ");
+                int index = Integer.parseInt(words[1]);
+                unmarkTask(index);
+            } else {
+                add(input);
+            }
+
+            speak("What's next?");
         }
 
-        speak("Bye. Hope to see you again soon!", true, false);
+        speak("\n---------- Bibo says: ----------");
+        speak("Bye. Hope to see you again soon!");
     }
 }
