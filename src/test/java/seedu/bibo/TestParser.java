6858096c18f6182700a46caf064dac3cd6f5d3c1
iuhiah <leehaihui2004@gmail.com>
2025-02-02 19:52:14 +0800

    Add JAR packaging to build file, fix styling issues

--- src/test/java/seedu/bibo/TestParser.java
+++ src/test/java/seedu/bibo/TestParser.java
@@ -1,7 +1,6 @@
 package seedu.bibo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Method;
 
